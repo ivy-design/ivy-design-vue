404573f22940b18f0a5b5fbc0eb61275ca691f60
GuoJikun <guojikun486546@163.com>
2023-03-06 16:09:34 +0800

    ✨ feat: 增加指令click-outside

--- packages/ui/src/utils/dom.ts
+++ packages/ui/src/utils/dom.ts
@@ -1,16 +1,17 @@
 /* istanbul ignore next */
 const trim = (string: string) => {
-    return (string || "").replace(/^[\s\uFEFF]+|[\s\uFEFF]+$/g, "");
+  return (string || "").replace(/^[\s\uFEFF]+|[\s\uFEFF]+$/g, "");
 };
 
 export function hasClass(el: HTMLElement, cls: string) {
-    if (!el || !cls) return false;
-    if (cls.indexOf(" ") !== -1) throw new Error("className should not contain space.");
-    if (el.classList) {
-        return el.classList.contains(cls);
-    } else {
-        return (" " + el.className + " ").indexOf(" " + cls + " ") > -1;
-    }
+  if (!el || !cls) return false;
+  if (cls.indexOf(" ") !== -1)
+    throw new Error("className should not contain space.");
+  if (el.classList) {
+    return el.classList.contains(cls);
+  } else {
+    return (" " + el.className + " ").indexOf(" " + cls + " ") > -1;
+  }
 }
 /**
  *
@@ -18,23 +19,23 @@ export function hasClass(el: HTMLElement, cls: string) {
  * @param {*} cls 类名
  */
 export function addClass(el: HTMLElement, cls: string) {
-    if (!el) return;
-    let curClass = el.className;
-    const classes = (cls || "").split(" ");
+  if (!el) return;
+  let curClass = el.className;
+  const classes = (cls || "").split(" ");
 
-    for (let i = 0, j = classes.length; i < j; i++) {
-        const clsName = classes[i];
-        if (!clsName) continue;
+  for (let i = 0, j = classes.length; i < j; i++) {
+    const clsName = classes[i];
+    if (!clsName) continue;
 
-        if (el.classList) {
-            el.classList.add(clsName);
-        } else if (!hasClass(el, clsName)) {
-            curClass += " " + clsName;
-        }
+    if (el.classList) {
+      el.classList.add(clsName);
+    } else if (!hasClass(el, clsName)) {
+      curClass += " " + clsName;
     }
-    if (!el.classList) {
-        el.className = curClass;
-    }
+  }
+  if (!el.classList) {
+    el.className = curClass;
+  }
 }
 
 /**
@@ -43,21 +44,25 @@ export function addClass(el: HTMLElement, cls: string) {
  * @param {*} cls 类名
  */
 export function removeClass(el: HTMLElement, cls: string) {
-    if (!el || !cls) return;
-    const classes = cls.split(" ");
-    let curClass = " " + el.className + " ";
+  if (!el || !cls) return;
+  const classes = cls.split(" ");
+  let curClass = " " + el.className + " ";
 
-    for (let i = 0, j = classes.length; i < j; i++) {
-        const clsName = classes[i];
-        if (!clsName) continue;
+  for (let i = 0, j = classes.length; i < j; i++) {
+    const clsName = classes[i];
+    if (!clsName) continue;
 
-        if (el.classList) {
-            el.classList.remove(clsName);
-        } else if (hasClass(el, clsName)) {
-            curClass = curClass.replace(" " + clsName + " ", " ");
-        }
+    if (el.classList) {
+      el.classList.remove(clsName);
+    } else if (hasClass(el, clsName)) {
+      curClass = curClass.replace(" " + clsName + " ", " ");
     }
-    if (!el.classList) {
-        el.className = trim(curClass);
-    }
+  }
+  if (!el.classList) {
+    el.className = trim(curClass);
+  }
 }
+export const isElement = (e: unknown): e is Element => {
+  if (typeof Element === "undefined") return false;
+  return e instanceof Element;
+};
