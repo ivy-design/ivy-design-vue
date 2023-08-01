ca2a359a684640bf19171e5636e3035854e51017
GuoJikun <guojikun486546@163.com>
2023-08-01 10:35:04 +0800

    🐳 chore(play): 修复无法预览

--- play/src/main.js
+++ play/src/main.js
@@ -2,5 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import IvyDesign from '@ivy-design/vue'
+import '@ivy-design/vue/dist/style.css'
 
-createApp(App).mount('#app')
+createApp(App).use(IvyDesign).mount('#app')
