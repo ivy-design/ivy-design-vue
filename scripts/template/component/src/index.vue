37009f2c8f3ffb2c1cd06b2df063c94ea66d521c
GuoJikun <qwerboy0912@gmail.com>
2023-11-24 15:44:53 +0800

    🐳 chore(全局): 优化生成组件的函数

---
+++ scripts/template/component/src/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { prefix } from '@/shared'
+
+defineOptions({
+  name: `${prefix}text`
+})
+
+const props = defineProps({
+  type: String
+})
+</script>
+
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
